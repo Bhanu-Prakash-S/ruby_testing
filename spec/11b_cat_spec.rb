4cd002879dd23c24a2974b92c6d0dc8db689f5f7
Bhanu-Prakash-S <bhanuprakash.simhadri@gmail.com>
2025-09-15 23:09:59 +0530

    9-11 lessons

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject (:puff) { described_class.new(name: 'Puff', breed: 'Influencer', color: 'black&white')}
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when cat and dog share a method name' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(puff).to_not be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(puff).to be_hiding
   end
 end
